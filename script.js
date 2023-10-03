ad27278b4e39828db4fedc708f7fa317c12a06cb
Srikanth Reddy <sriors123@gmail.com>
2023-10-03 09:23:11 +0530

    Removed JS Elements

--- script.js
+++ script.js
@@ -4,47 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
         londonButton.click();
     var londonButton2 = document.querySelector(".tablinks.def2:nth-child(1)");
         londonButton2.click();
-
-    // Example: Adding an event listener to a button
-    const myButton = document.getElementById('myButton');
-    myButton.addEventListener('click', function() {
-        alert('Button clicked!');
-    });
-
-    // Example: Fetching data from an API
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
-
-    // Example: Manipulating the DOM
-    const myElement = document.getElementById('myElement');
-    myElement.innerHTML = 'New content';
-    myElement.style.color = 'red';
-
-    // Example: Creating and appending elements
-    const newElement = document.createElement('div');
-    newElement.textContent = 'Newly created element';
-    document.body.appendChild(newElement);
-
-    // Example: Working with local storage
-    localStorage.setItem('myKey', 'myValue');
-    const storedValue = localStorage.getItem('myKey');
-    console.log('Stored value:', storedValue);
-
-    // Example: Handling form submissions
-    const myForm = document.getElementById('myForm');
-    myForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        const formData = new FormData(myForm);
-        const formObject = {};
-        formData.forEach((value, key) => {
-            formObject[key] = value;
-        });
-        console.log('Form data:', formObject);
-    });
-
-    
 });
 
 function openTab(evt, cityName, group) {

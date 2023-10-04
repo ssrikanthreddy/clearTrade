eab1a9a5119dc0ed77585e7abd150428fc88ad08
Srikanth Reddy <sriors123@gmail.com>
2023-10-04 23:06:22 +0530

    Pie chart now has colors

--- script.js
+++ script.js
@@ -227,8 +227,18 @@
             datasets: [{
                 label: 'Data',
                 data: [5, 10, 15, 20],
-                backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                borderColor: 'rgba(54, 162, 235, 1)',
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                ],
                 borderWidth: 1
             }]
         },

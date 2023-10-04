34b578463c1bcaa18cb6ced2b4cd62b9cf31daa3
Srikanth Reddy <sriors123@gmail.com>
2023-10-04 14:00:53 +0530

    Content positioning and line graph values add

--- script.js
+++ script.js
@@ -12,7 +12,7 @@
               labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
               datasets: [{
                   label: 'Data',
-                  data: [10, 20, 30, 40, 50],
+                  data: [10, 40, 15, 10],
                   backgroundColor: 'rgba(255, 99, 132, 0.2)',
                   borderColor: 'rgba(255, 99, 132, 1)',
                   borderWidth: 1

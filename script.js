1665dff3000c63b5f113e6c108e4a4dad144f2b0
Tanmay Adithya <marnitanmayadithya@gmail.com>
2023-10-04 20:01:52 +0530

    Merge with main again

--- script.js
+++ script.js
@@ -1,94 +1,83 @@
-  var chart1, chart2, chart3;
-  var dataArray = [0, 0, 0, 0, 0]; // Initialize with default values
+    var chart1, chart2, chart3;
+    var values = [];
+    var dataArray = [0, 0, 0, 0, 0]; // Initialize with default values
+
+    //Make vars for each input field
+    var supply1 = document.getElementById("supplier1");
+    var supply2 = document.getElementById("supplier2");
+
+    var importer1 = document.getElementById("importer1");
+    var imoprter2 = document.getElementById("importer2");
+    var imoprter3 = document.getElementById("importer3");
+
+    var retail1 = document.getElementById("retail1");
+    var retail2 = document.getElementById("retail2");
+
+    var customer1 = document.getElementById("customer1");
+
+
+    // Add event listeners to each input field
+    supply1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    supply2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    importer1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    imoprter2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    imoprter3.addEventListener("input", function(){
+        update1();
+        update2();
+        update3();
+    });
+
+    retail1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    retail2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    customer1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    // Get all the input elements
+    var inputFields = document.querySelectorAll('.input-field');
+
+  //Main function
   document.addEventListener('DOMContentLoaded', function() {
       // Code inside this block will run when the DOM is fully loaded
 
-      //Graph space START
-      // Initialize the first chart (Graph 1)
-      var ctx1 = document.getElementById('g1').getContext('2d');
-      chart1 = new Chart(ctx1, {
-          type: 'line',
-          data: {
-              labels: ['Label 1', 'Label 2', 'Label 3', 'Label 4', 'Label 5'],
-              datasets: [{
-                  label: 'Data',
-                  data: [10, 40, 15, 10],
-                  backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                  borderColor: 'rgba(255, 99, 132, 1)',
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: true
-                  }
-              }
-          }
-      });
-
-      // Initialize the second chart (Graph 2)
-      var ctx2 = document.getElementById('g2').getContext('2d');
-      chart2 = new Chart(ctx2, {
-          type: 'pie',
-          data: {
-              labels: ['Label A', 'Label B', 'Label C', 'Label D', 'Label E'],
-              datasets: [{
-                  label: 'Data',
-                  data: [5, 10, 15, 20, 25],
-                  backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                  borderColor: 'rgba(54, 162, 235, 1)',
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: true
-                  }
-              }
-          }
-      });
-
-        // Initialize the third chart (Graph 3)
-      var ctx3 = document.getElementById('g3').getContext('2d');
-      chart3 = new Chart(ctx3, {
-          type: 'bar',
-          data: {
-              labels: ['Label X', 'Label Y', 'Label Z'],
-              datasets: [{
-                  label: 'Data',
-                  data: [8, 16, 24],
-                  backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                  borderColor: 'rgba(75, 192, 192, 1)',
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  y: {
-                      beginAtZero: true
-                  }
-              }
-          }
-      });
-    //Graph Space end
+      //Load Graphs
+      graph1()
+      graph2()
+      graph3()
 
       console.log("Loaded OK");
-
-      //Make vars for each input field
-      var supply1 = document.getElementById("supplier1");
-      var supply2 = document.getElementById("supplier2");
-      var retail1 = document.getElementById("retail1");
-      var retail2 = document.getElementById("retail2");
-      var customer1 = document.getElementById("customer1");
-
-      //Add even listeners to each field var
-      supply1.addEventListener("input", update1);
-      supply2.addEventListener("input", update1);
-      retail1.addEventListener("input", update2);
-      retail2.addEventListener("input", update2);
-      customer1.addEventListener("input", update3);
 
       //Open tab by default
       var londonButton = document.querySelector(".tablinks.def:nth-child(1)");
@@ -119,50 +108,147 @@
   } 
 
   function updateData(){
-    dataArray[0] = document.getElementById("supplier1").value;
-    dataArray[1] = document.getElementById("supplier2").value;
-    dataArray[2] = document.getElementById("retail1").value;
-    dataArray[3] = document.getElementById("retail2").value;
-    dataArray[4] = document.getElementById("customer1").value;
+    // Loop through the input fields
+    for (var i = 0; i < inputFields.length; i++)
+    {
+        // Push the value of each input field into the array
+        dataArray[i] = (inputFields[i].value);
+        console.log(inputFields[i].value);
+    }
   }
 
 
   function update1() {
     updateData();
-    var profit = [];
 
-    //Profit 1
+    //Profit 1 : Supplier
     var making = dataArray[0];
     var selling = dataArray[1];
     var margin1 = (selling-making)*100/making;
 
-    //Profit 2
-    making = dataArray[2];
-    selling = dataArray[3];
+    //Profit 2 : Importer
+    making = dataArray[2] + dataArray[3];
+    selling = dataArray[4];
     var margin2 = (selling-making)*100/making;
 
-    //Profit 3
-    making = dataArray[4];
-    selling = making*0.75;
+    //Profit 3 : Retailer
+    making = dataArray[5];
+    selling = dataArray[6]
     var margin3 = (selling-making)*100/making;
     
     // Update the chart data and re-render
-    chart1.data.datasets[0].data = [margin1, margin2];
+    chart1.data.datasets[0].data = [margin1, margin2, margin3];
     chart1.update();
   }
   
+
   function update2() {
     updateData();
-    
+
+    var total = 0;
+    for (var i = 0; i < dataArray.length; i++){
+        total += dataArray[i];
+    }
+
+    //totals each
+    values[0] = (dataArray[0] + dataArray[1])*100/total;
+    values[1] = (dataArray[2] + dataArray[3] + dataArray[4])*100/total;
+    values[2] = (dataArray[5] + dataArray[6])*100/total;
+    values[3] = dataArray[7]*100/total;
+
     // Update the chart data and re-render
-    chart2.data.datasets[0].data = dataArray;
+    chart2.data.datasets[0].data = [values];
     chart2.update();
   }
   
+
   function update3() {
     updateData();
-  
+
+    var factors = [0.4, 0.3, 0.2, 0.1];
+    var risk = [];
+
+    risk[0] = factors[0]*(values[0]);
+    risk[1] = factors[1]*(values[1]);
+    risk[2] = factors[2]*(values[2]);
+    risk[3] = factors[3]*(values[3]);
+
     // Update the chart data and re-render
-    chart3.data.datasets[0].data = dataArray;
+    chart3.data.datasets[0].data = [risk];
     chart3.update();
+  }
+
+  function graph1(){
+
+    var ctx1 = document.getElementById('g1').getContext('2d');
+      chart1 = new Chart(ctx1, {
+          type: 'line',
+          data: {
+              labels: ['Supplier', 'Importer', 'Retailer'],
+              datasets: [{
+                  label: 'Data',
+                  data: [10, 40, 15],
+                  backgroundColor: 'rgba(255,255,0, 0.7)',
+                  borderColor: 'rgba(255,255,0, 0.7)',
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              scales: {
+                  y: {
+                      beginAtZero: true
+                  }
+              }
+          }
+      });
+  }
+
+  function graph2(){
+
+    var ctx2 = document.getElementById('g2').getContext('2d');
+    chart2 = new Chart(ctx2, {
+        type: 'pie',
+        data: {
+            labels: ['Supplier', 'Importer', 'Retailer', 'Customer'],
+            datasets: [{
+                label: 'Data',
+                data: [5, 10, 15, 20],
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+  }
+
+  function graph3(){
+
+    var ctx3 = document.getElementById('g3').getContext('2d');
+    chart3 = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: ['Supplier', 'Importer', 'Retailer', 'Customer'],
+            datasets: [{
+                label: 'Data',
+                data: [8, 16, 24, 12],
+                backgroundColor: 'rgba(75, 192, 200, 0.5)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
   }

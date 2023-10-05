4549b82feeedf52604fc7d8c5ec78cb1d9e15090
Srikanth Reddy <sriors123@gmail.com>
2023-10-05 22:43:14 +0530

    GPT Api calls working, gitignore updated

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,19 @@
-require("dotenv").config();
+require('dotenv').config();
 
-const {Configuration, OpenAIApi} = require("openai");
+const OpenAI = require("openai");
 
-const configuration = new Configuration({
-    apiKey = 
-})
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openFun = async() => {
+const chatCompletion = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user","content":"generate a small rhyming poem!",}],
+    max_tokens: 1
+        })
+    console.log(chatCompletion.choices[0].message.content);
+    }
+
+openFun()
+

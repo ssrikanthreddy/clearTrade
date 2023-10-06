e63f39aca1dd67c404288612698c5caeced7fc67
Srikanth Reddy <sriors123@gmail.com>
2023-10-07 00:37:23 +0530

    add

---
+++ CONTRIBUTING.md
@@ -0,0 +1,133 @@
+# Contributing to clearTrade
+
+***Welcome to the clearTrade project!*** 🚀 Thank you for considering contributing to our project. We appreciate your effort and value your contributions. To ensure a smooth and collaborative experience for everyone involved, please take a moment to review the following guidelines.
+
+## Table of Contents
+
+- [Development Environment Setup](#development-environment-setup)
+- [How Can I Contribute?](#how-can-i-contribute)
+  - [Bug Reports](#bug-reports)
+  - [Feature Requests](#feature-requests)
+  - [Pull Requests](#pull-requests)
+- [Code of Conduct](#code-of-conduct)
+- [License](#license)
+
+## Development Environment Setup
+
+To set up your development environment for contributing to clearTrade, please follow these steps:
+
+1. **Fork the clearTrade repository:** Click on the "Fork" button at the top right corner of the GitHub repository page to create a personal copy of the project in your own GitHub account.
+
+2. **Clone the repository:** Once you have forked the repository, clone it to your local machine using the following command in your terminal or command prompt:
+   ```
+   git clone https://github.com/srik04/clear_trade.git
+   ```
+
+3. **Navigate to the project directory:** Change your current directory to the cloned project directory:
+   ```
+   cd clear_trade
+   ```
+
+4. **Install dependencies:** Install the project dependencies by running the following command:
+   ```
+   npm i
+   ```
+
+5. **Set up any required environment variables:** If the project requires any environment variables, make sure to set them up according to the project's documentation.
+
+
+6. **Make your changes:** Start making your desired changes or additions to the codebase using your preferred code editor.
+
+7. **Test your changes:** Before submitting a pull request, make sure to test your changes thoroughly to ensure they work as expected and do not introduce any new issues.
+
+Once you have completed these steps, you are ready to contribute to clearTrade! Open a pull request with your changes, and the project maintainers will review your contribution.
+
+Thank you for your interest in contributing to clearTrade! We appreciate your support and look forward to collaborating with you to enhance our project.
+
+## How Can I Contribute?
+
+### Bug Reports
+
+If you encounter a bug or any unexpected behavior while using clearTrade, please help us by opening an issue on our GitHub repository. When creating a bug report, please provide detailed information about the problem, including steps to reproduce it. This will help us understand and address the issue effectively.
+
+#### Bug Report Template
+
+```
+## Bug Description
+
+A clear and concise description of the bug.
+
+## Steps to Reproduce
+
+1. Step 1
+2. Step 2
+3. Step 3
+   ...
+
+## Expected Behavior
+
+A clear and concise description of what you expected to happen.
+
+## Actual Behavior
+
+A clear and concise description of what actually happened.
+
+## Screenshots or Error Messages
+
+If applicable, add screenshots or error messages to help explain the issue.
+
+## Environment (if relevant)
+
+- Operating System: [e.g., Windows 10, macOS]
+- Browser: [e.g., Chrome, Firefox]
+- Version: [e.g., 92.0.4515.131]
+
+## Additional Information
+
+Add any other information about the problem here.
+```
+
+### Feature Requests
+
+We welcome your ideas and suggestions for improving clearTrade. If you have a feature request, please open an issue on our GitHub repository and provide a detailed description of the feature you would like to see. Explain why you believe the feature would be beneficial and provide any relevant examples or use cases.
+
+#### Feature Request Template
+
+```
+## Feature Description
+
+A clear and concise description of the feature you are requesting.
+
+## Motivation
+
+Explain why you believe this feature would be beneficial and how it addresses a specific need or problem.
+
+## Proposed Solution
+
+If you have any ideas on how this feature could be implemented or any specific requirements it should meet, please describe them here.
+
+## Alternatives Considered
+
+Have you considered any alternative solutions or workarounds? If so, please describe them here.
+
+## Additional Information
+
+Add any other information or context about the feature request here.
+```
+
+### Pull Requests
+
+If you'd like to contribute code to clearTrade, we encourage you to open a pull request. To do so, please follow these steps:
+
+1. Fork the clearTrade repository to your own GitHub account.
+2. Create a new branch for your changes.
+3. Implement your changes, adhering to the project's coding conventions and style guidelines.
+4. If applicable, include unit tests to ensure the integrity of your code.
+5. Document any changes or additions you've made, including updates to relevant documentation.
+6. Thoroughly test your changes to ensure they do not introduce any new issues.
+7. Open a pull request with a clear description of your changes. The project maintainers will review your pull request, provide feedback, and suggest any necessary modifications.
+
+
+## License
+
+By contributing to clearTrade, you agree that your contributions will be licensed under the same [License Name](LICENSE) as the project itself.
---
+++ scripts/script.js
@@ -0,0 +1,408 @@
+    var chart1, chart2, chart3;
+    var values = [];
+    var dataArray = [0, 0, 0, 0, 0]; // Initialize with default values
+
+    //Make vars for each input field
+    var supply1 = document.getElementById("supplier1");
+    var supply2 = document.getElementById("supplier2");
+
+    var importer1 = document.getElementById("importer1");
+    var imoprter2 = document.getElementById("importer2");
+    var imoprter3 = document.getElementById("importer3");
+
+    var retail1 = document.getElementById("retail1");
+    var retail2 = document.getElementById("retail2");
+
+    var customer1 = document.getElementById("customer1");
+
+
+    // Add event listeners to each input field
+    supply1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    supply2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    importer1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    imoprter2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    imoprter3.addEventListener("input", function(){
+        update1();
+        update2();
+        update3();
+    });
+
+    retail1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    retail2.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    customer1.addEventListener("input", function() {
+        update1();
+        update2();
+        update3();
+    });
+
+    // Get all the input elements
+    var inputFields = document.querySelectorAll('.input-field');
+
+//Main function
+document.addEventListener('DOMContentLoaded', function() {
+    // Code inside this block will run when the DOM is fully loaded
+
+    //Load Graphs
+    graph1()
+    graph2()
+    graph3()
+
+    console.log("Loaded OK");
+
+    //Open tab by default
+    var londonButton = document.querySelector(".tablinks.def");
+        londonButton.click();
+    var londonButton2 = document.querySelector(".tablinks.def2");
+        londonButton2.click();
+
+    //AiBotStuff
+    document.getElementById('chat-circle').addEventListener('click', function() {
+        // Get all the input fields
+        var inputFields = document.querySelectorAll('.input-field');
+    
+        // Create an object to store the input values
+        var inputData = {};
+    
+        // Loop through the input fields
+        inputFields.forEach(function(input) {
+          inputData[input.id] = input.value;
+        });
+
+        //Inputs
+        var s1 = supply1.value;
+        var s2 = supply2.value;
+
+        var i1 = importer1.value;
+        var i2 = imoprter2.value;
+        var i3 = imoprter3.value;
+        
+        var r1 = retail1.value;
+        var r2 = retail2.value;
+
+        var c1 = customer1.value;
+        
+    
+        // Print the input values to the console
+        console.log(`Summerize the financial situation of this supply 
+using the given information, numbers in rupees(money) and also find out 
+who's getting exploiting and who's is 
+the exploiter and if someone's profit margin 
+is too high: Supplier manufacturing cost is  ${inputData['supplier1']}
+and the supplier sells to importer at ${inputData['supplier2']},
+importer buys from supplier at${inputData['importer1']},
+importer pays ${inputData['importer2']} for transporting,
+importer sells from to retailer for ${inputData['importer3']},
+retailer buys from importer for${inputData['retail1']},
+retailer sells to customer for ${inputData['retail2']},
+customer buys from retailer for ${inputData['customer1']}.`);
+
+    });
+
+});
+
+  function openTab(evt, cityName, group) {
+      // Declare all variables
+      var i, tabcontent, tablinks;
+    
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName(group);
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+    
+      // Get all elements with class="tablinks" and remove the class "active"
+      tablinks = document.getElementsByClassName(group);
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+      }
+    
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(cityName).style.display = "flex";
+      evt.currentTarget.className += " active";
+  } 
+
+  function updateData(){
+    // Loop through the input fields
+    for (var i = 0; i < inputFields.length; i++)
+    {
+        // Push the value of each input field into the array
+        dataArray[i] = (inputFields[i].value);
+        //console.log(inputFields[i].value);
+    }
+  }
+
+
+  function update1() {
+    updateData();
+
+    //Profit 1 : Supplier
+    var making = dataArray[0];
+    var selling = dataArray[1];
+    var margin1 = (selling-making)*100/making;
+
+    //Profit 2 : Importer
+    making = dataArray[2] + dataArray[3];
+    selling = dataArray[4];
+    var margin2 = (selling-making)*100/making;
+
+    //Profit 3 : Retailer
+    making = dataArray[5];
+    selling = dataArray[6]
+    var margin3 = (selling-making)*100/making;
+    
+    // Update the chart data and re-render
+    chart1.data.datasets[0].data = [margin1, margin2, margin3];
+    chart1.update();
+  }
+  
+
+  function update2() {
+    // Assuming you have already defined and populated 'dataArray' and 'values'
+  
+    var total = 0;
+    for (var i = 0; i < dataArray.length; i++) {
+      total += dataArray[i];
+    }
+  
+    // Calculatepercentages and update 'values' array
+    values[0] = (dataArray[0] + dataArray[1]) * 100 / total;
+    values[1] = (dataArray[2] + dataArray[3] + dataArray[4]) * 100 / total;
+    values[2] = (dataArray[5] + dataArray[6]) * 100 / total;
+    values[3] = dataArray[7] * 100 / total;
+  
+    // Assuming 'chart2' is your Chart.js instance
+    // Update the chart data and re-render
+    chart2.data.datasets[0].data = values; // Remove the outer array brackets
+    chart2.update();
+  }
+  
+  function update3() {
+    updateData();
+  
+    var factors = [0.4, 0.3, 0.2, 0.1];
+    var risk = [];
+  
+    risk[0] = factors[0] * values[0];
+    risk[1] = factors[1] * values[1];
+    risk[2] = factors[2] * values[2];
+    risk[3] = factors[3] * values[3];
+  
+    // Update the chart data and re-render
+    chart3.data.datasets[0].data = risk; // Remove the outer array brackets
+    chart3.update();
+  }
+
+  function graph1() {
+    var ctx1 = document.getElementById('g1').getContext('2d');
+    chart1 = new Chart(ctx1, {
+      type: 'line',
+      data: {
+        labels: ['Supplier', 'Importer', 'Retailer','Customer'],
+        datasets: [{
+          label: 'Data',
+          data: [15, 35, 20,25],
+          backgroundColor:'rgb(14, 39, 20, 0.6)',
+        
+        borderColor: 'rgb(14, 39, 20, 0.6)',
+          borderWidth: 3,
+          pointRadius: 8, // Set the size of data points (markers)
+          pointHoverRadius: 20, // Set the size of data points on hover
+        }],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: true,
+        cutout: '40%',
+        layout: {
+          padding: {
+            bottom: 70 // Adjust the value to offset the chart to the top
+          },
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'bottom',
+          },
+        },
+        scales: {
+          y: {
+            beginAtZero: true,
+            ticks: {
+                color: 'rgb(14, 39, 20,1)',
+            }
+          },
+          x : {
+            ticks: {
+                color: 'rgb(14, 39, 20,1)',
+            }
+          }
+        },
+      },
+    });
+  }
+  
+  function graph2(){
+
+    var ctx2 = document.getElementById('g2').getContext('2d');
+    chart2 = new Chart(ctx2, {
+        type: 'pie',
+        data: {
+            labels: ['Supplier', 'Importer', 'Retailer', 'Customer'],
+            datasets: [{
+                label: 'Data',
+                data: [5, 10, 15, 20],
+                backgroundColor: [
+                    'rgba(29,64,47,0.4)',
+                    'rgba(83,134,142,0.4)',
+                    'rgba(124,170,194,0.4)',
+                    'rgba(175,208,245,0.4)',
+                ],
+                borderColor: [
+                    'rgba(29,64,47,255)',
+                    'rgba(83,134,142,255)',
+                    'rgba(124,170,194,255)',
+                    'rgba(175,208,245,255)',
+                ],
+                borderWidth: 0.5
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: true,
+            radius: '90%',
+            cutout: '0%',
+            layout: {
+                padding: {
+                    bottom: 70 // Adjust the value to offset the chart to the top
+                }
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'bottom'
+                }
+            },
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: 'rgb(14, 39, 20,1)',
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: 'rgb(14, 39, 20,1)',
+                    }
+                }
+            }
+        },
+        
+    });
+  }
+
+  function graph3(){
+
+    var ctx3 = document.getElementById('g3').getContext('2d');
+    chart3 = new Chart(ctx3, {
+        type: 'bar',
+        data: {
+            labels: ['Supplier', 'Importer', 'Retailer', 'Customer'],
+            datasets: [{
+                label: 'Data',
+                data: [8, 16, 24, 12],
+                backgroundColor: [
+                    'rgba(29,64,47,255)',
+                    'rgba(83,134,142,255)',
+                    'rgba(124,170,194,255)',
+                    'rgba(175,208,245,255)',
+                ],
+
+                borderColor: [
+                    'rgba(29,64,47,255)',
+                    'rgba(83,134,142,255)',
+                    'rgba(124,170,194,255)',
+                    'rgba(175,208,245,255)',
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    ticks: {
+                        color: 'rgb(14, 39, 20,1)',
+                    }
+                },
+                x: {
+                    ticks: {
+                        color: 'rgb(14, 39, 20,1)',
+                    }
+                }
+            },
+            layout: {
+                padding: {
+                    bottom: 70
+                }
+            },
+            plugins: {
+                legend: {
+                    display: true,
+                    position: 'top'
+                }
+            },
+            responsive: true,
+            maintainAspectRatio: true,
+            barPercentage: 1, // Adjust as needed
+            categoryPercentage: 0.5 // Adjust as needed
+        }
+    });
+  }
+
+    document.querySelector('#chat-circle').addEventListener('click', function() {
+    document.getElementById('chat-box').style.display = 'block';
+    document.querySelector('.chat-overlay').style.display = 'block';
+});
+
+document.querySelector('.chat-overlay').addEventListener('click', function() {
+    document.getElementById('chat-box').style.display = 'none';
+    document.querySelector('.chat-overlay').style.display = 'none';
+});
+
+
+function closeChat() {
+    document.getElementById("chat-box").style.display = "none";
+    document.querySelector('.chat-overlay').style.display = 'none';
+  }
+
+  
